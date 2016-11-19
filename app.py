0c297f3a0de27dc8218819709c239833f08b0814
torrescar <cmt57@duke.edu>
2016-11-19 16:24:48 -0500

    changed /search from ajax

--- app.py
+++ app.py
@@ -1,9 +1,63 @@
+# Copyright 2013 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+"""
+Sample App Engine application demonstrating how to connect to Google Cloud SQL
+using App Engine's native unix socket or using TCP when running locally.
 
+For more information, see the README.md.
+"""
+
+# [START all]
 from flask import Flask, render_template, request, json
+import os
 import MySQLdb
 
 app = Flask(__name__)
+
+# These environment variables are configured in app.yaml.
+CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
+CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
+CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
+CLOUDSQL_DB = os.environ.get('CLOUDSQL_DB')
+
+
+def connect_to_cloudsql():
+    # When deployed to App Engine, the `SERVER_SOFTWARE` environment variable
+    # will be set to 'Google App Engine/version'.
+    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+        # Connect using the unix socket located at
+        # /cloudsql/cloudsql-connection-name.
+        cloudsql_unix_socket = os.path.join(
+            '/cloudsql', CLOUDSQL_CONNECTION_NAME)
+
+        db = MySQLdb.connect(
+            unix_socket=cloudsql_unix_socket,
+            user=CLOUDSQL_USER,
+            passwd=CLOUDSQL_PASSWORD,
+            db=CLOUDSQL_DB)
+
+    # If the unix socket is unavailable, then try to connect using TCP. This
+    # will work if you're running a local MySQL server or using the Cloud SQL
+    # proxy, for example:
+    #
+    #   $ cloud_sql_proxy -instances=your-connection-name=tcp:3306
+    #
+    else:
+        db = MySQLdb.connect(
+            host='127.0.0.1', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
+    return db
 
 
 @app.route("/")
@@ -20,12 +74,11 @@ def search():
 
         # validate the received values
         if _dept and _num and _prof:
-            conn = MySQLdb.connect("104.198.135.7", "root", "rmcinstance", db="Classes")
-            print "did you get here?"
+            conn = connect_to_cloudsql()
             cursor = conn.cursor()
             #cursor.execute("SELECT * FROM Department")
             q = "INSERT INTO Department(id, name, abbr) VALUES (%s, %s, %s)"
-            cursor.execute(q, (2,'computerscience','cs'))
+            cursor.execute(q, (3,'documentarystudies','docst'))
             #cursor.execute("""INSERT INTO Department (id, name, abbr) VALUES (2, 'bleh', 'compsci')""")
             data = cursor.fetchall()
 
@@ -42,3 +95,5 @@ def search():
 
 if __name__ == "__main__":
     app.run()
+
+# [END all]
